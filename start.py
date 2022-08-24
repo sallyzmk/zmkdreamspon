52ec2003554627a9133601390d1fdc7381ba29b9
sallyzmk <sallyzmk@naver.com>
2022-08-24 17:11:11 +0900

    updated

---
+++ start.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+import pandas as pd 
+from sqlalchemy import create_engine
+import psycopg2
+
+
+## DB 연결 
+def db_create():
+    engine = create_engine("postgresql://axliyegodnehag:9ef8dab9260e8b0c8642f9ad7dca998ccc3f53760f6ad3285310e5f0973b9c06@ec2-44-205-112-253.compute-1.amazonaws.com:5432/dcdfbmcp09gmcc", echo = False)
+
+    engine.connect()    
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS dreamspon(
+            name varchar(90) NOT NULL,
+            advantage varchar(10) NOT NULL,
+            who varchar(40) NOT NULL,
+            age int NOT NULL,
+            where1 VARCHAR(30) NOT NULL,
+            qualification VARCHAR(30) NOT NULL,
+            url VARCHAR(70) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/dreamspon.csv')
+    print(data)
+    data.to_sql(name='dreamspon', con=engine, schema = 'public', if_exists='replace', index=False)
+
+
+def db_select(choice,choice1,choice2,choice3,choice4):
+    conn = psycopg2.connect(host="ec2-44-205-112-253.compute-1.amazonaws.com", dbname="dcdfbmcp09gmcc", user="axliyegodnehag", password="9ef8dab9260e8b0c8642f9ad7dca998ccc3f53760f6ad3285310e5f0973b9c06")
+    # heroku에 배포되어 있는 데이터베이스에 접속하기
+    
+    cur = conn.cursor()
+    # choice="\'%%생활비지원%%'"
+    # choice1="\'%%대학생%%'"
+    # choice2=25
+    # choice3="\'%%서울%%'"
+    # choice4="\'%%기초수급자%%'"
+    cur.execute("SELECT name,url FROM dreamspon WHERE advantage LIKE {0} AND who like {1} AND (age IS null OR age < {2}) AND (where1 IS null or where1 LIKE {3}) AND (qualification IS null or qualification LIKE {4})".format(choice,choice1,choice2,choice3,choice4))
+    rows = cur.fetchall() 
+    # list 타입
+    # 데이터내용 전부 불러서 rows에 입력
+    
+    df = pd.DataFrame(rows, columns = ['name','url'])
+    # DataFrame으로 만들어주기
+    # 컬럼명을 지정
+    return df
+    
+
+
+
+def db_select1():
+    list=[]
+    # choice="\'생활비지원'"
+    # result= engine.execute("SELECT name,url FROM dreamspon WHERE who LIKE'%%대학생%%'")
+    result= engine.execute("SELECT name,url FROM dreamspon WHERE (age IS null OR age < 25) AND who LIKE '%%대학생%%' AND (where1 LIKE '%%서울%%' OR where1 IS null) AND (qualification LIKE '%%기초수급자%%' OR qualification IS null) AND advantage LIKE '%%학비지원%%'")
+    
+    for r in result: 
+        print(r)
+             
+        
+    
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # db_create()
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    db_create()
+    # db_select()
+    # db_select1()
+    app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from app.main import app
+from start import db_create
 
 if __name__ == "__main__":
+    db_create()
     app.run(threaded=True, port=5000)
