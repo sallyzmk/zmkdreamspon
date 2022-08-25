9e3983cf86025dc3ac7d113bffdd5b6bce22fdb6
sallyzmk <sallyzmk@naver.com>
2022-08-25 10:27:05 +0900

    update

--- start.py
+++ start.py
@@ -4,13 +4,13 @@ from flask import Flask
 import pandas as pd 
 from sqlalchemy import create_engine
 import psycopg2
+engine = create_engine("postgresql://kczlivirywphmd:92be7484071849e5bdc99cde3148129fafb4c480a15b0fa66a0c2838d8ef7988@ec2-3-219-19-205.compute-1.amazonaws.com:5432/d52u3agrcchahb", echo = False)
 
+engine.connect()
 
 ## DB 연결 
 def db_create():
-    engine = create_engine("postgresql://axliyegodnehag:9ef8dab9260e8b0c8642f9ad7dca998ccc3f53760f6ad3285310e5f0973b9c06@ec2-44-205-112-253.compute-1.amazonaws.com:5432/dcdfbmcp09gmcc", echo = False)
-
-    engine.connect()    
+    
     engine.execute("""
         CREATE TABLE IF NOT EXISTS dreamspon(
             name varchar(90) NOT NULL,
@@ -19,7 +19,8 @@ def db_create():
             age int NOT NULL,
             where1 VARCHAR(30) NOT NULL,
             qualification VARCHAR(30) NOT NULL,
-            url VARCHAR(70) NOT NULL
+            url VARCHAR(70) NOT NULL,
+            image VARCHAR(100) NOT NULL
         );"""
     )
     data = pd.read_csv('data/dreamspon.csv')
@@ -27,8 +28,10 @@ def db_create():
     data.to_sql(name='dreamspon', con=engine, schema = 'public', if_exists='replace', index=False)
 
 
+
+
 def db_select(choice,choice1,choice2,choice3,choice4):
-    conn = psycopg2.connect(host="ec2-44-205-112-253.compute-1.amazonaws.com", dbname="dcdfbmcp09gmcc", user="axliyegodnehag", password="9ef8dab9260e8b0c8642f9ad7dca998ccc3f53760f6ad3285310e5f0973b9c06")
+    conn = psycopg2.connect(host="ec2-3-219-19-205.compute-1.amazonaws.com", dbname="d52u3agrcchahb", user="kczlivirywphmd", password="92be7484071849e5bdc99cde3148129fafb4c480a15b0fa66a0c2838d8ef7988")
     # heroku에 배포되어 있는 데이터베이스에 접속하기
     
     cur = conn.cursor()
